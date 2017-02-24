20a11a7696ed0e10bef62cbcd0ba8b3782b7cdc9
Carlo Rosati <carlorosati@users.noreply.github.com>
2017-02-24 11:18:08 -0500

    add bookmarklet

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript: (function () {var url = document.getElementsByClassName("listenArtworkWrapper")[0].getElementsByClassName("image__full")[0].getAttribute("style").match(/url\(([^)]*)/)[1];window.open(url.replace("t500x500","original"));}());
